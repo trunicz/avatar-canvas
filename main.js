f0ff17bfaf6074d16a93a6c98698277b1e3090af
Trunicz <romelin2001@hotmail.com>
2022-08-24 13:44:30 -0600

    generador - canvas

--- main.js
+++ main.js
@@ -1,26 +1,690 @@
 const c = document.querySelector('#mycanvas')
 const ctx = c.getContext('2d')
+// const selector = document.querySelector('#selector')
+// const brush = document.querySelector('#selector-brush')
+// const sizebrush = document.querySelector('#size')
+
+// let typebrush = 1
+// let color = 'red'
+// let size = 10
+
+// selector.addEventListener('change', () => {
+//   color = selector.value
+// })
+
+// brush.addEventListener('change', () => {
+//   typebrush = parseInt(brush.value)
+// })
+
+// sizebrush.addEventListener('change', () => {
+//   size = parseInt(sizebrush.value)
+// })
+
+// c.addEventListener('click', (e) => {
+//   const x = e.offsetX
+//   const y = e.offsetY
+//   if (typebrush === 1) {
+//     ctx.fillStyle = color
+//     ctx.fillRect(x - size / 2, y - size / 2, size, size)
+//     console.log(`ctx.fillStyle = "${color}"`)
+//     console.log(
+//       `ctx.fillRect(${x - size / 2} , ${y - size / 2}, ${size}, ${size})`
+//     )
+//   } else if (typebrush === 2) {
+//     ctx.fillStyle = color
+//     ctx.strokeStyle = color
+//     ctx.lineWidth = 5
+//     // empezamos a dibujar
+//     ctx.beginPath()
+//     ctx.moveTo(x, y)
+//     ctx.arc(x, y, size / 3, 0, 2 * Math.PI)
+//     ctx.fill()
+//     ctx.stroke()
+
+//     console.log(`ctx.fillStyle = "${color}"`)
+//     console.log(`ctx.strokeStyle = "${color}"`)
+//     console.log('ctx.lineWidth = 5')
+//     console.log('ctx.beginPath()')
+//     console.log(`ctx.arc(${x},${y}, ${size / 3}, 0, 2 * Math.PI)`)
+//     console.log('ctx.fill()')
+//     console.log('ctx.stroke()')
+//   }
+// })
+
+// c.addEventListener('mousemove', (e) => {
+//   const x = e.offsetX
+//   const y = e.offsetY
+//   console.log(`x = ${x} || y = ${y}`)
+//   console.log(typebrush)
+// })
+
+ctx.fillStyle = 'blue'
+ctx.fillRect(-258, -274, 1000, 1000)
+ctx.fillStyle = 'green'
+ctx.fillRect(-81, 357, 250, 250)
+ctx.fillStyle = 'green'
+ctx.fillRect(82, 353, 250, 250)
+ctx.fillStyle = 'green'
+ctx.fillRect(234, 353, 250, 250)
+ctx.fillStyle = 'green'
+ctx.fillRect(337, 355, 250, 250)
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
 ctx.beginPath()
-ctx.arc(250, 150, 40, 0, 2 * Math.PI)
+ctx.arc(28, 45, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
 ctx.stroke()
-
-// ctx.beginPath()
-// ctx.arc(250, 260, 50, 0, 2 * Math.PI)
-// ctx.stroke()
-
-// ctx.moveTo(220, 150)
-// ctx.lineTo(280, 150)
-// ctx.lineTo(280, 250)
-// ctx.lineTo(220, 250)
-// ctx.lineTo(220, 150)
-// ctx.moveTo(212, 291)
-// ctx.lineTo(170, 385)
-// ctx.lineTo(210, 385)
-// ctx.lineTo(250, 311)
-// ctx.stroke()
-
-c.addEventListener('mousemove', (e) => {
-  const x = e.offsetX
-  const y = e.offsetY
-  console.log(`x = ${x} -- y = ${y}`)
-})
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(28, 56, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(44, 56, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(60, 48, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(47, 35, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(30, 74, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(19, 87, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(47, 78, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(66, 67, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(78, 51, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(82, 29, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(104, 30, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(225, 28, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(241, 40, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(250, 27, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(269, 48, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(252, 65, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(236, 61, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(224, 50, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(272, 66, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(288, 49, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(265, 30, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(290, 25, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(138, 136, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(166, 143, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(161, 121, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(147, 108, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(133, 117, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(121, 139, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(117, 164, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(139, 157, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(363, 73, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(381, 74, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(397, 78, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(416, 79, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(434, 87, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(413, 97, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(394, 107, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(376, 102, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(363, 118, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(356, 98, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(427, 114, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'red'
+ctx.fillRect(210, 272, 100, 100)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(206, 255, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(218, 257, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(233, 252, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(248, 254, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(263, 255, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(278, 253, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(294, 257, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(307, 254, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(219, 239, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(234, 228, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(243, 215, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(254, 207, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(264, 222, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(278, 232, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(292, 244, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(244, 225, 40, 40)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(234, 243, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(279, 250, 20, 20)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(241, 303, 30, 30)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(241, 329, 30, 30)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(241, 344, 30, 30)
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(247, 337, 3.3333333333333335, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'blue'
+ctx.fillRect(280, 301, 20, 20)
+ctx.fillStyle = 'blue'
+ctx.fillRect(217, 300, 20, 20)
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(5, 377, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(14, 380, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(27, 380, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(39, 379, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(53, 376, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(70, 372, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(87, 367, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(93, 357, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(86, 358, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'black'
+ctx.strokeStyle = 'black'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(79, 365, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(7, 367, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(18, 371, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(34, 373, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(49, 371, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(62, 367, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(68, 362, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(79, 360, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(88, 357, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(93, 348, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(85, 342, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(74, 336, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(60, 331, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(48, 329, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(37, 327, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(24, 326, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(15, 324, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(7, 324, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(5, 331, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(13, 332, 6.666666666666667, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(8, 345, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(33, 347, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(57, 353, 16.666666666666668, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'green'
+ctx.lineWidth = 5
+ctx.beginPath()
+ctx.arc(76, 348, 3.3333333333333335, 0, 2 * Math.PI)
+ctx.fill()
+ctx.stroke()
+ctx.fillStyle = 'yellow'
+ctx.fillRect(14.5, 362.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(43.5, 362.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(77.5, 352.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(69.5, 338.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(44.5, 331.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(58.5, 353.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(28.5, 350.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(23.5, 326.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(11.5, 324.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(6.5, 343.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(3.5, 361.5, 5, 5)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(12.5, 343.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(48.5, 366.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(28.5, 367.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(47.5, 347.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(24.5, 334.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(44.5, 339.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(55.5, 335.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(84.5, 339.5, 5, 5)
+ctx.fillStyle = 'red'
+ctx.fillRect(7.5, 332.5, 5, 5)
